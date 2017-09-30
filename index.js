e72360148466967ea86db30fd35d652b27ee1caa
Simon Sundström <ssundstrom@live.se>
2017-09-30 10:30:13 +0200

    search node done

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
-  clientId : 'c11d380eadd04921a083d5637c108f8c',
-  clientSecret : 'f8db5475fa7748e28203dd0ebac181e4',
-  redirectUri : ''
+    clientId : 'c11d380eadd04921a083d5637c108f8c',
+    clientSecret : 'f8db5475fa7748e28203dd0ebac181e4',
+    redirectUri : ''
+});
+
+spotifyApi.clientCredentialsGrant()
+.then(function(data) {
+  console.log('The access token expires in ' + data.body['expires_in']);
+  console.log('The access token is ' + data.body['access_token']);
+
+  // Save the access token so that it's used in future calls
+  spotifyApi.setAccessToken(data.body['access_token']);
+}, function(err) {
+      console.log('Something went wrong when retrieving an access token', err);
 });
 
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')  
+})
+
+app.get('/search/:name', function(req, res) {
+    spotifyApi.searchTracks(req.params.name)
+        .then(function(data) {
+            res.send(data)
+        }, function(err) {
+        console.error(err);
+        });
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
