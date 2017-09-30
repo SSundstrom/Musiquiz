a3d51739f9bf0b86a607d09b9dd59105c7d944f6
Jesper Lindström <raxezdev@gmail.com>
2017-09-30 14:22:34 +0200

    Progress again

--- frontend/src/pages/PlayerGuess.js
+++ frontend/src/pages/PlayerGuess.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import { search as trackSearch } from '../api';
 
 class PlayerGuess extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit="">
           <label>
             Enter a song name
-            <input type="text" />
+            <input 
+              type="text" 
+              onChange={(e) => this.onChange(e.currentTarget.value)} 
+              value={this.state.value}
+            />
           </label>
-          <input type="submit" value="Join" />
-        </form>
       </div>
     );
+  }
+
+  onChange(value) {
+    this.setState({
+      value: value
+    });
+
+    console.log(value);
+
+    setTimeout(() => {
+      if (this.state.value === value) {
+        this.search(value);
+      }
+    }, 50);
+  }
+
+  search(value) {
+    if (value.length < 2) {
+      return this.setState({
+        results: []
+      });
+    }
+
+    trackSearch(value, function(results) {
+      console.log(results);
+    });
   }
 }
 
