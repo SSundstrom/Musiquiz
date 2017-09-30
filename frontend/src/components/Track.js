6a1a025e3dfbe9db4ac5a069a2d9e7eba9b813ad
SamuelHakansson <samuel.hakansson@live.com>
2017-09-30 20:00:33 +0200

    track file

---
+++ frontend/src/components/Track.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const Track = (props) => (
+  <div className="coverArt">
+        <div className="trackitem">
+          <img src={props.track.album.images[2].url}/>
+          <div className="trackinfo">
+            <div className="trackname"> {props.track.name} </div>
+            <div className="trackartists"> {props.track.artists.map(artist => <span>{artist.name }</span>)} </div>
+          </div>
+        </div>
+  </div>
+);
+
+export default Track;
