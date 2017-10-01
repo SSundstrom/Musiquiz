20e1903ab15407fd01c2630fe391a6490d04d179
Jesper Lindström <raxezdev@gmail.com>
2017-10-01 14:43:25 +0200

    Score update

--- frontend/src/components/Scores.js
+++ frontend/src/components/Scores.js
@@ -1,13 +1,25 @@
 import * as React from 'react';
 
-const Scores = (props) => (
-  <div className="scores">
-      {Object.keys(props.score).map((name)=>(
-        <div key={name}>
-          <div>{name === props.nickname && '-->'}{name} - {props.score[name]}</div>
-        </div>
-      ))}
-  </div>
-);
+class Scores extends React.Component {
+  render() {
+    return (
+      <div className="scores">
+        {Object.keys(this.props.score).sort((a, b) => this.props.score[b]-this.props.score[a]).map((name) => (
+          <div key={name}>
+            <div className="score-row">
+              <div className="score-name">
+                {name}
+                <small>{name === this.props.nickname && ' (you)'}</small>
+              </div>
+              <div className="score-score">
+                {this.props.score[name]}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Scores;
