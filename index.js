af04ac96db94cdf4fc78886dbce1ac99a2310527
Mikael Lönn <mikael.lonn@chorus.se>
2019-02-28 09:25:00 +0100

    added .env

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const SpotifyWebApi = require('spotify-web-api-node');
-
+require('dotenv').load();
 // credentials are optional
 const spotifyApi = new SpotifyWebApi({
-  clientId: 'c11d380eadd04921a083d5637c108f8c',
-  clientSecret: 'f8db5475fa7748e28203dd0ebac181e4',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
   redirectUri: '',
 });
 
