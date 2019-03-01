acbd23978e9a43d996dd7b517e15d7e130b9ee96
Mikael Lönn <mikael.lonn@chorus.se>
2019-03-01 18:00:59 +0100

    adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: ['prettier'],
+  rules: {
+    'no-console': 'off',
+    'object-curly-newline': 'off',
+    'no-unused-vars': 'off',
+    'no-param-reassign': 'off',
+  },
+};
