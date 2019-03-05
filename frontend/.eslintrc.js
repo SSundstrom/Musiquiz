80c0e0ca2fc69abfb626091bd7f6776a498d5af2
Mikael Lönn <mikael.lonn@chorus.se>
2019-03-05 11:21:10 +0100

    eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'no-underscore-dangle': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
     'no-alert': 'off',
     'no-console': 'off',
     'object-curly-newline': 'off',
     'no-unused-vars': 'off',
     'no-param-reassign': 'off',
     'react/jsx-filename-extension': 'off',
-    'react/forbid-prop-types': 'off'
-  }
+    'react/forbid-prop-types': 'off',
+  },
 };
