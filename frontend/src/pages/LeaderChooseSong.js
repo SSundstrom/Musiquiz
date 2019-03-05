817731308942b87a21b71c1cda40d937dbbeba50
Mikael Lönn <mikael.lonn@chorus.se>
2019-03-05 11:33:57 +0100

    removed unused vars

--- frontend/src/pages/LeaderChooseSong.js
+++ frontend/src/pages/LeaderChooseSong.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { search as trackSearch } from '../api';
-import Track from '../components/Track';
-import RecommendedSongs from '../components/RecommendedSongs';
 import Search from '../components/Search';
 
 const LeaderChooseSong = ({ onSelectSong, name }) => (
