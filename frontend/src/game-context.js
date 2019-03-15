5be36e580ad5ce487fd2a4155439ab79ced3f08c
Mikael Lönn <mikael.lonn@outlook.com>
2019-03-15 11:20:39 +0100

    Feature/im feeling lucky (#42)
    * adds feeling lucky button
    
    * adds feeling lucky button
    
    Closes #32

--- frontend/src/game-context.js
+++ frontend/src/game-context.js
@@ -18,6 +18,7 @@ const initialState = {
   guessTimer: null,
   isLeader: false,
   leader: null,
+  joined: false,
   correctSong: null,
   songToPlay: null,
   guessed: false,
@@ -44,7 +45,9 @@ class GameProvider extends Component {
         emit('join', { ...session });
       }
     });
-
+    on('lucky', nickname => {
+      this.setState({ nickname });
+    });
     on('playerDisconnected', player => {
       if (player) {
         this.setState({ players: this.updatePlayers(player) });
@@ -64,14 +67,18 @@ class GameProvider extends Component {
       });
     });
 
-    on('joinSuccess', ({ nickname, foundRoom }) => {
-      const { leader } = foundRoom;
+    on('joinSuccess', ({ nickname, room }) => {
+      const { leader } = room;
       this.setState({
         nickname,
+        joined: true,
         isLeader: leader ? leader.nickname === nickname : false,
-        ...foundRoom,
+        ...room,
       });
-      if (foundRoom.gamestate === 'midgame') {
+      const session = cookies.get('session');
+      session.nickname = nickname;
+      cookies.set('session', session);
+      if (room.gamestate === 'midgame') {
         this.startGuessTimer();
       }
     });
@@ -100,6 +107,7 @@ class GameProvider extends Component {
           started: false,
           guessTimer: null,
           isLeader: false,
+          joined: false,
           leader: null,
           correctSong: null,
           songToPlay: null,
@@ -147,6 +155,7 @@ class GameProvider extends Component {
         started: false,
         guessTimer: null,
         isLeader: false,
+        joined: false,
         leader: null,
         correctSong: null,
         songToPlay: null,
@@ -233,13 +242,18 @@ class GameProvider extends Component {
     }, 1000);
   }
 
+  lucky(name) {
+    console.log(name);
+    emit('lucky', name);
+  }
+
   joinAsPlayer(nickname, name) {
     const { cookies } = this.props;
     const session = cookies.get('session');
     if (!session || session.name !== name) {
       const tomorrow = new Date();
       tomorrow.setDate(tomorrow.getDate() + 1);
-      cookies.set('session', { sessionId: uuidv4(), nickname, name }, { path: '/', expires: tomorrow });
+      cookies.set('session', { sessionId: uuidv4(), name }, { path: '/', expires: tomorrow });
     }
     if (!nickname.length) {
       return;
@@ -291,6 +305,7 @@ class GameProvider extends Component {
           onSelectSong: song => this.selectSong(song),
           onSaveSettings: time => this.sendSettings(time),
           onShowSettings: () => this.onShowSettings(),
+          lucky: name => this.lucky(name),
         }}
       >
         {children}
